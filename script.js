0e9aa9508460291ee2d2f66d603197bc47722df1
Ian F <ian.ferg36@gmail.com>
2021-03-29 22:08:42 -0600

    set up fetch, set up wire frame hours ago but forgot to push

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello Ian");
+var searchForm = $("#search-form");
+var citySearchEl = $("#city-search");
+
+searchForm.on("submit", function (event) {
+  event.preventDefault();
+  var citySearch = citySearchEl.val();
+  console.log(citySearch);
+  var apiKey = "2810f23e0f72acb2e940047beb55e002";
+
+  var apiUrl =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    citySearch +
+    "&appid=" +
+    apiKey;
+
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+});
